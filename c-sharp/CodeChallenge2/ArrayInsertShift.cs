9c108023150389f07e65f76b6ee80e695794ce4a
Kristian Dotson <KDotson387@gmail.com>
2023-07-10 15:12:11 -0500

    added challeneg 1

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -4,8 +4,20 @@ namespace CodeChallenges
     {
         public static int[] InsertShiftArray(int[] array, int value)
         {
-            //insert the value into the middle of the array
-            return array;
+      int midIndex = array.Length / 2;
+      int[] newArray = new int[array.Length + 1];
+      for (int i = 0; i < midIndex; i++)
+      {
+        newArray[i] = array[i];
+      }
+      newArray[midIndex] = value;
+      for (int i = midIndex + 1; i < newArray.Length; i++)
+      {
+        newArray[i] = array[i - 1];
+      }
+
+      //insert the value into the middle of the array
+      return newArray;
         }
     }
 }
