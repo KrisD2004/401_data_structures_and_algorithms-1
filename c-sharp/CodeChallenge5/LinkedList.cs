bbf96bad1d169c190fff5b9727bfdb270321b416
Kristian Dotson <KDotson387@gmail.com>
2023-07-20 13:18:58 -0500

    finished 5

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,13 +1,102 @@
 namespace CodeChallenges 
 {
-    public class LinkedList
+  /* public class LinkedList
+   {
+       // public Node Head {get; set;}
+
+   }
+
+   public class Node
+   {
+
+   }*/
+
+  public class Node
+  {
+    public Node? Next { get; set; }
+    public int Value { get; set; }
+
+
+
+    public Node Last { get; set; }
+
+    public Node(int value)
     {
-        // public Node Head {get; set;}
+      Value = value;
+      Next = null;
+      Last = null;
+    }
+  }
+
+
+  public class LinkedList
+
+  {
+    public Node? Head { get; set; }
+
+    public Node Tail { get; set; }
+    //Miles "Tails" Prower
+    public Node CurrentNode { get; set; }
+
+    public LinkedList()
+    {
+      Head = null;
+    }
+
+    public void Insert(int value)
+    {
+
+      Node newNode = new Node(value);
+
+      if (Head == null)
+      {
+
+        Head = newNode;
+
+      }
+      else
+      {
+
+        newNode.Next = Head;
+        Head = newNode;
+
+      }
+
+
+
 
     }
 
-    public class Node
+    public bool Includes(int value)
     {
+      bool foundValue = false;
+      Node currentNode = Head;
+      while (currentNode != null)
+      {
+        if (currentNode.Value == value)
+        {
+          foundValue = true;
 
+        }
+        currentNode = currentNode.Next;
+      }
+      return foundValue;
     }
+
+
+    public string ToString()
+    {
+      string listString = "";
+      Node currentNode = Head;
+
+      while (currentNode != null)
+      {
+        listString += String.Format("{0} -> ", currentNode.Value);
+        currentNode = currentNode.Next;
+      }
+
+      return listString + "NULL";
+    }
+  }
+
 }
